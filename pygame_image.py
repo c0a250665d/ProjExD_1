03d63db98ad22a7c18de7151742afc081416d4c9
katoshunta <c0a250665d@edu.teu.ac.jp>
2026-04-14 16:44:48 +0900

    演習１

--- pygame_image.py
+++ pygame_image.py
@@ -25,6 +25,7 @@ def main():
         screen.blit(bg_img2, [-x + 1600, 0])
         screen.blit(bg_img, [-x + 3200, 0])
         screen.blit(kk_img,kk_rct)
+        kk_rct.move_ip((-1,0))
         key_lst = pg.key.get_pressed()
         if key_lst[pg.K_UP]:
             kk_rct.move_ip((0,-1))
@@ -33,7 +34,7 @@ def main():
         if key_lst[pg.K_LEFT]:
             kk_rct.move_ip((-1,0))
         if key_lst[pg.K_RIGHT]:
-            kk_rct.move_ip((+1,0))
+            kk_rct.move_ip((+2,0))
         pg.display.update()
         tmr += 1        
         clock.tick(200)
